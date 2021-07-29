1d2519162b750b9f8853262787a7fc6c7a8a0c6d
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-07-29 19:35:57 +1000

    Init backend

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (_, res) => {
+	res.status(200).send();
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
