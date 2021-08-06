f7095147a38ea4ea6f7577bfe664f8308e9bc44d
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-08-06 22:40:40 +1000

    CI test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (_, res) => {
-	res.status(200).send("hello world");
+	res.status(200).send("henlo world");
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
