e8dbf521c9b435808e67ad01d8ef516c99405a66
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-08-07 16:57:01 +1000

    fix: correctly configure serving of static files in deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express = require("express");
+import cors = require("cors");
 import path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
+require("dotenv").config();
 
 app.get("/api/", (_, res) => {
     res.status(200).send("rebuild");
@@ -14,6 +16,11 @@ if (process.env.NODE_ENV === "production") {
     app.get("/*", function (req, res) {
         res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
     });
-}
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+    app.listen(port, () => console.log(`Running on port ${port}`));
+} else {
+    // cors for communicating with react frontend (only in development)
+    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+    app.listen(port, () => console.log(`Running on http://localhost:${port}`));
+}
