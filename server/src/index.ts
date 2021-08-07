c1c5692af3c3845d2075098f3937ffd6dc859149
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-08-07 15:32:31 +1000

    feat: configure client and server to server static files when deployed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,19 @@
 import express = require("express");
+import path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (_, res) => {
     res.status(200).send("rebuild");
 });
+
+// Serve static frontend files (only when deployed)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../client/build")));
+
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+    });
+}
 
 app.listen(port, () => console.log(`Running on port ${port}`));
