cd7ac661f2f768c2aded9928d066132fc3aa6284
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-08-07 15:15:07 +1000

    test: trigger heroku rebuild

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (_, res) => {
-    res.status(200).send("heroku rebuild");
+    res.status(200).send("rebuild");
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
