fef08213925648aac6f39ff47e8a4c27723a11a1
Alec <alecyu99@gmail.com>
2021-08-21 22:34:32 +1000

    login and register functional components made, kiwi img added

---
+++ client/src/components/registerForm.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import kiwi from "../images/kiwi-3.svg"
+import styles from "./Styles.module.css"
+
+function Register(){
+    return(
+        <div className="base-container">
+            <div className="header">Login Page</div>
+
+            <div className="content">
+
+                <div className="image">
+                    <img className={styles.image} src={kiwi}/>
+                </div>
+
+                <div className="form">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" placeholder="username"></input>
+                </div>
+
+                <div className="form">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="password"></input>
+                </div>
+
+            </div>
+
+            <div className="footer">
+                <button className="btn" type="button">Login</button>
+
+            </div>
+
+
+
+        </div>
+    )
+
+    
+}
+
+
+export default Register
