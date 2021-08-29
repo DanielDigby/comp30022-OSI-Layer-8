09285c5cc0e5b379d2a952dc78e2a998a8229d55
XRAYRAY22 <88013109+XRAYRAY22@users.noreply.github.com>
2021-08-29 22:30:16 +1000

    feat: set up base css classes and note view
    setup menu items

---
+++ client/src/views/NotesView/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import styles from "./NotesView.module.css";
+
+// Semantic UI button
+import { Button, Menu } from 'semantic-ui-react'
+
+const NotesView = (): JSX.Element => {
+    // api call
+    return (
+        <div>
+            <div className={styles.container}>
+                <Menu secondary vertical>
+                    <Menu.Item
+                        name="Pinned"
+                    />
+                    <Menu.Item
+                        name="Events"
+                    />
+                    <Menu.Item
+                        name="Contacts"
+                    />
+                </Menu>
+            </div>
+        </div>
+    );
+};
+export default NotesView;
+
