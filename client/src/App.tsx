f172ecc3663e6085e11c7ac50279ac50c7aff162
Alec <alecyu99@gmail.com>
2021-08-30 20:28:15 +1000

    added NotesView files to refer to for structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import styles from "./App.module.css";
+
 // import our views
 import BaseView from "./views/BaseView";
-import LogIn from "./components/LogIn"
-import Register from "./components/Register"
+import NotesView from "./views/NotesView";
 
 const App = (): JSX.Element => {
     return (
-        <div>
-            <LogIn />
+        <div 
+            id="ipad-container"
+            className="container-center-horizontal">
+            <Switch>
+                {/* add more routes here, path is the url you want on the frontend component is the view to be rendered */}
+                <Route path="/notes" component={NotesView} />
+                <Route path="/" component={BaseView} />
+            </Switch>
         </div>
     );
 };
