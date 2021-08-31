f692e6899e2fc68da51d72c4870284cae5c1b489
Ying Wang <wayw2@student.unimelb.edu.au>
2021-08-31 21:32:35 +1000

    feat: temporary changes for notes

---
+++ client/src/components/Note/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Note.module.css";
+
+const Note = (): JSX.Element => {
+    // api call
+    return (
+        <div>
+            <div className={styles.container}>hello world</div>
+        </div>
+    );
+};
+export default Note;
