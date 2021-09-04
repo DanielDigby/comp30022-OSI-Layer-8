561364eb870c4e2427d6abdb88c73b1b3164f74f
Fiona <fiona.zhu@student.unimelb.edu.au>
2021-09-04 21:39:50 +1000

    feat: route for updating user profile

--- server/src/modules/user/userController.ts
+++ server/src/modules/user/userController.ts
@@ -24,6 +24,20 @@ const createUser = async (req: express.Request, res: express.Response) => {
     }
 }
 
+// controller for updating a user's profile details
+const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const id = req.params.Id;
+        const newData  = req.body;
+        const note = await User.findByIdAndUpdate(id, newData).setOptions({ new: true, overwrite: true });
+
+        return res.status(200).send(note);
+    } catch (err) {
+        return res.send(err);
+    }
+}
+
 module.exports = {
     createUser,
+    updateUser
 };
--- server/src/modules/user/userRouter.ts
+++ server/src/modules/user/userRouter.ts
@@ -7,4 +7,7 @@ const userRouter = express.Router();
 // create a new note in database
 userRouter.post("/", express.json(), userController.createUser);
 
+// update a user's details
+userRouter.put('/:Id', express.json(), userController.updateUser);
+
 module.exports = userRouter;
