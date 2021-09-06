0868617992642a29453cdc2511107103567b8c6b
Fiona <fiona.zhu@student.unimelb.edu.au>
2021-09-06 16:59:14 +1000

    fix: move json middleware to index.ts

--- server/src/modules/user/userRouter.ts
+++ server/src/modules/user/userRouter.ts
@@ -5,9 +5,9 @@ const userRouter = express.Router();
 
 
 // create a new note in database
-userRouter.post("/", express.json(), userController.createUser);
+userRouter.post("/", userController.createUser);
 
 // update a user's details
-userRouter.put('/:Id', express.json(), userController.updateUser);
+userRouter.put('/:Id', userController.updateUser);
 
 module.exports = userRouter;
