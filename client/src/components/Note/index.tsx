ad26e7422fffc53df4e05a0b999e91e7433687e2
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-09-09 20:40:52 +1000

    feat: add tagName to Tag props

--- client/src/components/Note/index.tsx
+++ client/src/components/Note/index.tsx
@@ -1,30 +1,26 @@
 import React from "react";
 import styles from "./Note.module.css";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image } from "semantic-ui-react";
 
 // Using this now oop change the <Route path="/" component={Note} /> line too
-//import Tag from "./components/Tag";
-
+import Tag from "../Tag";
 
 const Note = (): JSX.Element => {
     // api call
     return (
-        <div className={styles.Card} > 
+        <div className={styles.Card}>
             <Card>
                 <Card.Content>
                     <Card.Header>Note Heading</Card.Header>
                     <Card.Description>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </Card.Description>
-                    
                 </Card.Content>
-                    
                 <Card.Content extra>
                     <a>
-                        <Icon name='tag' />
-                        Contact
+                        <Tag tagName="Event" />
                     </a>
                 </Card.Content>
             </Card>
--- client/src/components/Tag/index.tsx
+++ client/src/components/Tag/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import styles from "./Note.module.css";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
-import { Icon } from 'semantic-ui-react'
+import { Icon } from "semantic-ui-react";
 
-const Tag = (): JSX.Element => {
+type TagProps = {
+    tagName: string;
+};
+
+const Tag = ({ tagName }: TagProps): JSX.Element => {
     // api call
     return (
         <div>
-            <Icon name='tag' />
-            Contact 
+            <Icon name="tag" />
+            {tagName}
         </div>
     );
 };
 export default Tag;
-
-
-
