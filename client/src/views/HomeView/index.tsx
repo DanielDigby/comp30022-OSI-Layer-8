0bfd001ffb73ce8655c4c169594c8d350dd05143
Alec <alecyu99@gmail.com>
2021-09-09 21:29:33 +1000

    prettier formatting

--- client/src/views/HomeView/index.tsx
+++ client/src/views/HomeView/index.tsx
@@ -15,26 +15,29 @@ const HomeView = (): JSX.Element => {
     // api call
     return (
         <div className={styles.container}>
-                <div className="image">
-                    <img className={styles.image} src={logo} />
-                </div>
-                
-                <h1 className={styles.heading}>Cara</h1>
-                <h4 className={styles.heading2}>Untangle your personal life</h4>
+            <div className="image">
+                <img className={styles.image} src={logo} />
+            </div>
+
+            <h1 className={styles.heading}>Cara</h1>
+            <h4 className={styles.heading2}>Untangle your personal life</h4>
 
             <div className={styles.button}>
-                <Button className={styles.button} basic colour="black" content="Sign-In" 
+                <Button
+                    className={styles.button}
+                    basic
+                    colour="black"
+                    content="Sign-In"
                     onClick={() => navigateLogin()}
                 />
-                <Button basic colour="black" content="Register" 
+                <Button
+                    basic
+                    colour="black"
+                    content="Register"
                     onClick={() => navigateRegister()}
                 />
             </div>
         </div>
-
-
-
-
     );
 };
 
