0dca3f1cf3d5a7a88312031af5913cf3c0bc4852
Alec <alecyu99@gmail.com>
2021-09-12 21:37:17 +1000

    added segments for colour choices, need to work out changing colours

--- client/src/views/SettingsView/ColourBlocks.tsx
+++ client/src/views/SettingsView/ColourBlocks.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./ColourBlocks.module.css";
 
-import { Grid } from "semantic-ui-react";
+import { Segment } from "semantic-ui-react";
 
 const ColourBlocks = (): JSX.Element => {
     return (
         <div className={styles.blocks}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
+            <div className={styles.block}>
+                <Segment raised className={styles.colour}>
+                    dark
+                </Segment>
+            </div>
+            <div className={styles.block}>
+                <Segment raised className={styles.colour}>
+                    <div className={styles.dark}>ey</div>
+                </Segment>
+            </div>
         </div>
     );
 };
