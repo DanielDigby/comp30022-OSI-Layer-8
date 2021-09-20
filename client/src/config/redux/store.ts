17cba096df2abad144fd6e3690c8fff384724f95
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-09-20 20:09:38 +1000

    fix: adjust naming

--- client/src/config/redux/store.ts
+++ client/src/config/redux/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-import notesReducer from "./userSlice";
+import noteReducer from "./noteSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        notes: notesReducer,
+        notes: noteReducer,
     },
 });
 
