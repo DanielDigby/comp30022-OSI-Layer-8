9b513954405107184e41c4900aeb908914d97d54
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-09-20 20:18:28 +1000

    fix: correctly configure redux offline deps

--- client/src/config/redux/store.ts
+++ client/src/config/redux/store.ts
@@ -1,12 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, StoreEnhancer } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import noteReducer from "./noteSlice";
+import { offline } from "@redux-offline/redux-offline";
+import config from "@redux-offline/redux-offline/lib/defaults";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         notes: noteReducer,
     },
+    enhancers: [offline(config) as StoreEnhancer],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
