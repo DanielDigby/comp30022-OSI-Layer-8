7e1ee95bc39306003dc9334a3f5b0795de3655de
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-09-21 16:59:43 +1000

    feat: adjust example test to avoid linting errors

--- client/src/tests/Example.test.tsx
+++ client/src/tests/Example.test.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import HomeView from "../views/HomeView";
+import LogInView from "../views/LogInView";
 
 test("check that jest is working", () => {
     expect(1).toBe(1);
 });
 
-// test("renders BaseView", () => {
-//     render(<HomeView />);
-//     const textElement = screen.getByText(/cara/i);
-//     expect(textElement).toBeInTheDocument();
-// });
+test("renders BaseView", () => {
+    render(<LogInView />);
+    const textElement = screen.getByText(/cara/i);
+    expect(textElement).toBeInTheDocument();
+});
