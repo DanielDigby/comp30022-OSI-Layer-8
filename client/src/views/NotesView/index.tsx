aa0749a9a58ada7cde1cb3994b5da4040dd418bd
Alec <alecyu99@gmail.com>
2021-09-26 07:31:49 +1000

    changing every column in notessection to a list --> easier to manage

--- client/src/views/NotesView/index.tsx
+++ client/src/views/NotesView/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
 
 // Semantic UI button
@@ -20,22 +21,39 @@ import { DropdownItem } from "semantic-ui-react";
     /* Package to help with smooth drag n drop features */
 }
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { types } from "@babel/core";
 
 {
     /* SAMPLE NOTES DATA USED FOR DRAG AND DROP TESTING */
 }
 
 const testNotes = [
-    { title: "col1", items: ["note1", "note2", "note3"] },
-    { title: "col2", items: ["note4", "note5"] },
-    { title: "col3", items: ["note7"] },
-    { title: "col4", items: [] },
+    {
+        id: 1,
+        name: "note1",
+    },
+    {
+        id: 2,
+        name: "note2",
+    },
+    {
+        id: 3,
+        name: "note3",
+    },
+    {
+        id: 4,
+        name: "note4",
+    },
+    {
+        id: 5,
+        name: "note5",
+    },
 ];
-
 const NotesView = (): JSX.Element => {
     const history = useHistory();
     const navigateDashboard = () => history.push("/dashboard");
 
+    const [notes, updateNotes] = useState(testNotes);
     // api call
 
     return (
@@ -51,44 +69,15 @@ const NotesView = (): JSX.Element => {
             </div>
 
             {/* Main notes area? */}
-            <DragDropContext onDragEnd={(result) => console.log(result)}>
-                <Droppable droppableId="notes">
-                    {(provided) => (
-                        <div
-                            className={dndStyles.notesSection}
-                            {...provided.droppableProps}
-                            ref={provided.innerRef}
-                        >
-                            {testNotes.map((col, index) => (
-                                <div
-                                    key={col.title}
-                                    className={dndStyles.column}
-                                >
-                                    {col.items.map((item) => (
-                                        <Draggable
-                                            key={item}
-                                            draggableId={item}
-                                            index={index}
-                                        >
-                                            {(provided) => (
-                                                <div
-                                                    className={dndStyles.item}
-                                                    {...provided.draggableProps}
-                                                    ref={provided.innerRef}
-                                                    {...provided.dragHandleProps}
-                                                >
-                                                    {item}
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                </div>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
+            <div className={dndStyles.notesSection}>
+                <div className={dndStyles.column}>
+                    {testNotes.map((note) => (
+                        <li key={note.id} className={dndStyles.item}>
+                            {note.name}
+                        </li>
+                    ))}
+                </div>
+            </div>
 
             {/* Searchbar on top right corner */}
             <div className={styles.containerRight}>
