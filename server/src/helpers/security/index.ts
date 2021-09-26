a60e985e9eae399ba760ce811da8fb2f213d8b68
Fiona <fiona.zhu@student.unimelb.edu.au>
2021-09-26 16:10:14 +1000

    feat: add jwt authentication to protect note routes

--- server/src/helpers/security/index.ts
+++ server/src/helpers/security/index.ts
@@ -2,6 +2,8 @@ import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
 import { IUser } from "../../modules/user/userModel";
 import { IRequestWithCookie } from "../../interfaces/expressInterfaces";
+import express from "express";
+
 
 export function validatePassword(password1: string, password2: string) {
     return bcrypt.compareSync(password1, password2);
@@ -29,4 +31,26 @@ export function extractJwt(req: IRequestWithCookie) {
         token = req.cookies["jwt"];
     }
     return token;
+}
+
+// Verify the jwt token
+export function verifyJwt(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const token = extractJwt(req);
+
+    if (token) {
+        jsonwebtoken.verify(token, "secret", (err, decodedToken) => {
+            if (err) {
+                // Redirect to login page if the JWT is unauthenticated
+                console.log(err);
+                res.redirect('/login');
+            } else {
+                console.log(token);
+                console.log(decodedToken);
+                next();
+            }
+        })
+    } else {
+        // Redirect to login page if there no JWT
+        res.redirect('/login');
+    }
 }
--- server/src/modules/note/noteRouter.ts
+++ server/src/modules/note/noteRouter.ts
@@ -1,21 +1,22 @@
 import express from "express";
+import { verifyJwt } from "../../helpers/security/index"
 
 const noteController = require("./noteController");
 const noteRouter = express.Router();
 
 // get all notes in database
-noteRouter.get("/", noteController.getNotes);
+noteRouter.get("/", verifyJwt, noteController.getNotes);
 
 // get a specific note in database
-noteRouter.get("/:Id", noteController.getNote);
+noteRouter.get("/:Id", verifyJwt, noteController.getNote);
 
 // create a new note in database
-noteRouter.post("/", noteController.postNote);
+noteRouter.post("/", verifyJwt, noteController.postNote);
 
 // update a note
-noteRouter.put("/:Id", noteController.putNote);
+noteRouter.put("/:Id", verifyJwt, noteController.putNote);
 
 // delete a note
-noteRouter.delete("/:Id", noteController.deleteNote);
+noteRouter.delete("/:Id", verifyJwt, noteController.deleteNote);
 
 module.exports = noteRouter;
