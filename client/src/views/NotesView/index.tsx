e9120cca5060159cd66456bea365dc7f82d39d9b
Alec <alecyu99@gmail.com>
2021-09-26 19:26:09 +1000

    drag and drop works, no state implemented yet for multiple columns

--- client/src/views/NotesView/index.tsx
+++ client/src/views/NotesView/index.tsx
@@ -28,32 +28,32 @@ import { types } from "@babel/core";
 }
 
 const testNotes = [
+    { id: "1", content: "note1" },
+    { id: "2", content: "note2" },
+];
+
+const testNotes2 = [
+    { id: "3", content: "note3" },
+    { id: "4", content: "note4" },
+];
+
+const testColumns = [
     {
-        id: 1,
-        name: "note1",
+        name: "col1",
+        items: testNotes,
     },
     {
-        id: 2,
-        name: "note2",
-    },
-    {
-        id: 3,
-        name: "note3",
-    },
-    {
-        id: 4,
-        name: "note4",
-    },
-    {
-        id: 5,
-        name: "note5",
+        name: "col2",
+        items: testNotes2,
     },
 ];
+
 const NotesView = (): JSX.Element => {
     const history = useHistory();
     const navigateDashboard = () => history.push("/dashboard");
 
-    const [notes, updateNotes] = useState(testNotes);
+    /* State for our drag and drop */
+    const [columns, updateColumns] = useState(testColumns);
     // api call
 
     return (
@@ -70,13 +70,66 @@ const NotesView = (): JSX.Element => {
 
             {/* Main notes area? */}
             <div className={dndStyles.notesSection}>
-                <div className={dndStyles.column}>
-                    {testNotes.map((note) => (
-                        <li key={note.id} className={dndStyles.item}>
-                            {note.name}
-                        </li>
-                    ))}
-                </div>
+                <DragDropContext onDragEnd={(result) => console.log(result)}>
+                    {Object.entries(testColumns).map(([id, column]) => {
+                        return (
+                            <Droppable droppableId={id} key={id}>
+                                {(provided, snapshot) => {
+                                    return (
+                                        <div
+                                            {...provided.droppableProps}
+                                            ref={provided.innerRef}
+                                            className={dndStyles.column}
+                                        >
+                                            {/* Inside each column, now spawn all the note items */}
+                                            {column.items.map((item, index) => {
+                                                return (
+                                                    <Draggable
+                                                        key={item.id}
+                                                        draggableId={item.id}
+                                                        index={index}
+                                                    >
+                                                        {(
+                                                            provided,
+                                                            snapshot
+                                                        ) => {
+                                                            return (
+                                                                <div
+                                                                    ref={
+                                                                        provided.innerRef
+                                                                    }
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}
+                                                                    className={
+                                                                        dndStyles.item
+                                                                    }
+                                                                    style={{
+                                                                        backgroundColor:
+                                                                            snapshot.draggingOver
+                                                                                ? "#263B4A"
+                                                                                : "#456C86",
+                                                                        ...provided
+                                                                            .draggableProps
+                                                                            .style,
+                                                                    }}
+                                                                >
+                                                                    {
+                                                                        item.content
+                                                                    }
+                                                                </div>
+                                                            );
+                                                        }}
+                                                    </Draggable>
+                                                );
+                                            })}
+                                            {provided.placeholder}
+                                        </div>
+                                    );
+                                }}
+                            </Droppable>
+                        );
+                    })}
+                </DragDropContext>
             </div>
 
             {/* Searchbar on top right corner */}
