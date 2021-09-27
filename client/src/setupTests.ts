a2c504fd8a620c735df4e27b5fb6edf9d36b64d0
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-09-27 16:53:07 +1000

    fix: remove setup test unused code

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -4,28 +4,3 @@
 // learn more: https://github.com/testing-library/jest-dom
 /* eslint-disable */
 import "@testing-library/jest-dom";
-
-class LocalStorageMock {
-    store: Record<string, unknown>;
-    constructor() {
-        this.store = {};
-    }
-    clear() {
-        this.store = {};
-    }
-    getItem(key: string) {
-        return this.store[key] || null;
-    }
-    setItem(key: string, value: any) {
-        this.store[key] = value.toString();
-    }
-    removeItem(key: string) {
-        delete this.store[key];
-    }
-}
-
-const localStorage = new LocalStorageMock();
-
-// https://github.com/redux-offline/redux-offline/issues/308
-// jest test environment expects these to exist
-window.localStorage = localStorage;
