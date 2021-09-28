b86525add360065c3c126a97675f384f1c747a25
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-09-28 15:12:53 +1000

    feat: add log out test

--- client/src/helpers/api/users.test.ts
+++ client/src/helpers/api/users.test.ts
@@ -1,4 +1,4 @@
-import { logInAPI } from "./users";
+import { logInAPI, logOutAPI } from "./users";
 import { RESET_STATE as RESET_OFFLINE } from "@redux-offline/redux-offline/lib/constants";
 import { store, RESET_BASE } from "../../config/redux/store";
 import axios from "axios";
@@ -69,6 +69,77 @@ describe("Users API Helpers", () => {
                 expect(axios.get).toHaveBeenCalledTimes(1);
                 expect(user).toMatchObject(apiUser);
                 expect(notes).toEqual(userNotes);
+            }
+        );
+    });
+
+    describe("Log out user", () => {
+        beforeEach(() => {
+            store.dispatch({ type: RESET_OFFLINE });
+            store.dispatch({ type: RESET_BASE });
+        });
+
+        it(
+            "When a user is logged in, and the outbox is empty:\n" +
+                "\t clear redux offline\n" +
+                "\t clear the redux store\n",
+            async () => {
+                store.dispatch({
+                    type: "Offline/STATUS_CHANGED",
+                    payload: {
+                        online: true,
+                    },
+                });
+
+                const apiUser = {
+                    email: "test@email.com",
+                    firstName: "firstName",
+                    lastName: "lastName",
+                    password: "redacted",
+                    profilePic: "somepicurl",
+                    colourScheme: "PLACEHOLDER",
+                    tags: [],
+                };
+                const userNotes = [
+                    {
+                        title: "NEW NOTE TEST",
+                        _clientId: "75072f66-3b31-40f7-b3b7-5e46f4ea93fc",
+                        tags: [],
+                        relatedNotes: [],
+                        _id: "61514289e3c2e405ab49db7e",
+                    },
+                    {
+                        title: "SECOND NOTE",
+                        _clientId: "75072f77-3b31-40f7-b3b7-5e46f4ea9abc",
+                        tags: [],
+                        relatedNotes: [],
+                        _id: "61514289e3c2e405ab4abcde",
+                    },
+                ];
+                const mockRes = { status: 200, data: apiUser };
+                (axios.post as unknown as jest.Mock).mockResolvedValueOnce(
+                    mockRes
+                );
+                const mockRes2 = { status: 200, data: userNotes };
+                (axios.get as unknown as jest.Mock).mockResolvedValueOnce(
+                    mockRes2
+                );
+                logInAPI({ email: "test@email.com", password: "password1" });
+                await new Promise((r) => setTimeout(r, 50));
+
+                const mockRes3 = { status: 200 };
+                (axios.get as unknown as jest.Mock).mockResolvedValueOnce(
+                    mockRes2
+                );
+
+                logOutAPI();
+
+                await new Promise((r) => setTimeout(r, 50));
+                const user = store.getState().user.account;
+                const notes = store.getState().notes.array;
+                expect(axios.get).toHaveBeenCalledTimes(2);
+                expect(user).toBe(null);
+                expect(notes).toEqual([]);
             }
         );
     });
