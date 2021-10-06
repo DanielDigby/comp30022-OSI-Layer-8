b950260816daab94897ecf213335ced9afd2922a
yingyingyw <yingw239@gmail.com>
2021-10-07 05:50:19 +1100

    loaded specific number of notes to dashboard

--- client/src/views/DashboardView/index.tsx
+++ client/src/views/DashboardView/index.tsx
@@ -6,7 +6,8 @@ import ProfileImage from "../NotesView/ProfileImage";
 import { store } from "../../config/redux/store";
 
 import { filterNotes, FilterOn } from "../../helpers/utils/filter";
-import { INote } from "../../interfaces/note";
+import { INote, NoteModes } from "../../interfaces/note";
+import Note from "../../components/Note";
 
 const DashboardView = (): JSX.Element => {
     const history = useHistory();
@@ -19,19 +20,125 @@ const DashboardView = (): JSX.Element => {
     });
 
     /* get the First name of User */
-    const firstName = store.getState().user.firstName;
+    //const firstName = store.getState().user.firstName;
+
+    const oct = new Date();
+    oct.setFullYear(2021, 10, 31);
+    const nov = new Date();
+    nov.setFullYear(2021, 11, 20);
+    const dec = new Date();
+    dec.setFullYear(2021, 12, 10);
+
+    const allNotes: Array<INote> = [
+        {
+            _id: "sdjfasdfa",
+            _clientId: "sfhkjasd",
+            title: "event-october",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: oct,
+            pinned: false,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "alvndslks",
+            _clientId: "hdjaasdsdakjasd",
+            title: "event-december",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: dec,
+            pinned: false,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "alsdkfasd",
+            _clientId: "hdjafsaasd",
+            title: "no event or pin",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: null,
+            pinned: false,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "aslslfvjd",
+            _clientId: "sfhkjasd",
+            title: "event-november",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: nov,
+            pinned: false,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "sdhfjhas",
+            _clientId: "hdjaasdsdakjasd",
+            title: "pinned-one",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: null,
+            pinned: true,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "sfkadfklhasdfa",
+            _clientId: "hdjafsaasd",
+            title: "pinned-two",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: null,
+            pinned: true,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "sfkadfknxmxcnx",
+            _clientId: "hdjafsaasd",
+            title: "pinned-three",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: null,
+            pinned: true,
+            tags: [],
+            relatedNotes: [],
+        },
+        {
+            _id: "sfjsncohslcmx",
+            _clientId: "hdjafsaasd",
+            title: "pinned-four",
+            text: "test",
+            image: "test",
+            reminderTime: null,
+            eventTime: null,
+            pinned: true,
+            tags: [],
+            relatedNotes: [],
+        },
+    ];
 
     /* store.getState the arrays of notes */
-    const allNotes = store.getState().notes.array;
-    const eventNotes = filterNotes(allNotes, FilterOn.EVENT_TIME);
+    //const allNotes = store.getState().notes.array;
+    let eventNotes = filterNotes(allNotes, FilterOn.EVENT_TIME);
     if (eventNotes.length > 3) {
-        eventNotes.slice(0, 4);
+        eventNotes = eventNotes.slice(0, 3);
     }
 
-    /* Filter the Pinned Notes, up to 4 */
-    const pinnedNotes = filterNotes(allNotes, FilterOn.PINNED);
-    if (pinnedNotes.length > 4) {
-        pinnedNotes.slice(0, 5);
+    /* Filter the Pinned Notes, up to 3 */
+    let pinnedNotes = filterNotes(allNotes, FilterOn.PINNED);
+    if (pinnedNotes.length > 3) {
+        pinnedNotes = pinnedNotes.slice(0, 3);
     }
 
     return (
@@ -48,7 +155,7 @@ const DashboardView = (): JSX.Element => {
                 <div className={styles.greetingsContainer}>
                     <div className={styles.heading}>
                         <h1>Good Morning,</h1>
-                        <h1>{firstName}</h1>
+                        <h1>Sonja</h1>
                     </div>
 
                     <div className={styles.date}>
@@ -74,15 +181,31 @@ const DashboardView = (): JSX.Element => {
                     <Icon name="calendar alternate" size="big" />
                 </div>
                 <div className={styles.eventsContainer}>
-                    {eventNotes.map(function (content, idx) {
-                        return <li key={idx}>{content._id}</li>;
+                    {eventNotes.map((note: INote) => {
+                        return (
+                            <Note
+                                note={note}
+                                mode={NoteModes.STANDARD}
+                                key={note._id}
+                            />
+                        );
                     })}
                 </div>
 
                 <div className={styles.tumbtackContainer}>
                     <Icon name="thumbtack" size="big" />
                 </div>
-                <div className={styles.pinnedContainer}></div>
+                <div className={styles.pinnedContainer}>
+                    {pinnedNotes.map((note: INote) => {
+                        return (
+                            <Note
+                                note={note}
+                                mode={NoteModes.STANDARD}
+                                key={note._id}
+                            />
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
