291c938e5e0e294dcb880db9231a5a9051de1412
XRAYRAY22 <88013109+XRAYRAY22@users.noreply.github.com>
2021-10-08 00:24:32 +1100

    Added ability to read and store user inputs

--- client/src/views/NotesView/Menu.tsx
+++ client/src/views/NotesView/Menu.tsx
@@ -3,6 +3,7 @@ import styles from "./NotesView.module.css";
 import globalStyles from "../../App.module.css";
 
 import { useHistory } from "react-router-dom";
+import { addTagAPI } from "../../helpers/api/tags";
 
 // Semantic UI button
 import { Menu, Icon, Input } from "semantic-ui-react";
@@ -11,9 +12,14 @@ const MenuItem = (): JSX.Element => {
     const navHistory = useHistory();
     const navigateSettings = () => navHistory.push("/settings");
     const [show, setShow] = useState(false);
+    const [tag, setTag] = useState("");
+
+    const handleTag = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTag(e.target.value);
+    };
 
     return (
-        <div className={styles.sideContainer}>
+        <div className={styles.containerLeft}>
             <div>
                 <Icon name="tag" size="large" />
             </div>
@@ -28,8 +34,19 @@ const MenuItem = (): JSX.Element => {
             <div>
                 <Icon name="plus" size="large" onClick={() => setShow(!show)} />
             </div>
-            <div>{show ? <Input placeholder="New Tag..." /> : null}</div>
-
+            <div className={styles.input}>
+                {show ? (
+                    <Input
+                        action={{
+                            icon: "plus",
+                            onClick: () => (addTagAPI(tag), setShow(false)),
+                        }}
+                        h
+                        placeholder="New Tag"
+                        onChange={handleTag}
+                    />
+                ) : null}
+            </div>
             <div>
                 <Icon
                     name="cog"
