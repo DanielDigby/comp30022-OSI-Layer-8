3d1af166277f4079188f3ef8a6145f80c8f36d8a
Daniel Digby <daniel.a.digby@gmail.com>
2021-10-09 23:13:09 +1100

    fix: remove unused useEffect

--- client/src/views/DashboardView/index.tsx
+++ client/src/views/DashboardView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleIsNewLogin } from "../../config/redux/userSlice";
 import styles from "./DashboardView.module.css";
