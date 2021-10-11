fbb4484f15e5bfb08c24e9b72252ad431dbb01f7
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-11 14:37:03 +1100

    feat: add home view back at /test url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import LogInView from "./views/LogInView";
 import RegisterView from "./views/RegisterView";
 import DashboardView from "./views/DashboardView";
 import SettingsView from "./views/SettingsView";
+import HomeView from "./views/HomeView";
 
 const App = (): JSX.Element => {
     return (
@@ -17,6 +18,7 @@ const App = (): JSX.Element => {
                 {/* add more routes here, path is the url you want on the frontend component is the view to be rendered */}
                 <Route exact path="/" component={DashboardView} />
                 <Route path="/login" component={LogInView} />
+                <Route path="/test" component={HomeView} />
                 <Route path="/register" component={RegisterView} />
                 <Route path="/settings" component={SettingsView} />
                 <Route path="/notes" component={NotesView} />
