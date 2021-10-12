012b98fc3cf23f4739d01841a5e8abe34c751b4c
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-12 21:23:56 +1100

    feat: restructure order of operations mapping notes to columns

--- client/src/helpers/utils/columns.ts
+++ client/src/helpers/utils/columns.ts
@@ -1,5 +1,3 @@
-/* eslint-disable security/detect-object-injection */
-// dictionary access injection sink only used with internally generated val
 import { ColumnDict } from "../../views/NotesView/DnD";
 import { INote } from "../../interfaces/note";
 import _ from "lodash";
@@ -7,36 +5,38 @@ import { v4 as uuid } from "uuid";
 
 export const mapNotesToColumns = (notes: Array<INote>): ColumnDict => {
     const temp = _.cloneDeep(notes);
-    const id1 = uuid();
-    const id2 = uuid();
-    const id3 = uuid();
-    const newDict = {
-        [id1]: {
-            name: "col1",
-            items: new Array<INote>(),
-        },
-        [id2]: {
-            name: "col2",
-            items: new Array<INote>(),
-        },
-        [id3]: {
-            name: "col3",
-            items: new Array<INote>(),
-        },
-    };
+    const arr1 = [];
+    const arr2 = [];
+    const arr3 = [];
 
     while (temp.length !== 0) {
         if (temp.length % 3 === 0) {
             const note = temp.shift();
-            if (note) newDict[id3].items.push(note);
+            if (note) arr3.push(note);
         }
         if (temp.length % 2 === 0) {
             const note = temp.shift();
-            if (note) newDict[id2].items.push(note);
+            if (note) arr2.push(note);
         } else {
             const note = temp.shift();
-            if (note) newDict[id1].items.push(note);
+            if (note) arr1.push(note);
         }
     }
+
+    const newDict = {
+        [uuid()]: {
+            name: "col1",
+            items: arr1,
+        },
+        [uuid()]: {
+            name: "col2",
+            items: arr2,
+        },
+        [uuid()]: {
+            name: "col3",
+            items: arr3,
+        },
+    };
+
     return newDict;
 };
