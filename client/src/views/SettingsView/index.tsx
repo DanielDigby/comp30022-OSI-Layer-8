f83dded4d06ac186b9639faa69260d4ebcbf7f25
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-14 17:13:55 +1100

    feat: tidy up settings.ts

--- client/src/views/SettingsView/index.tsx
+++ client/src/views/SettingsView/index.tsx
@@ -1,21 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
-import { capitalize, cloneDeep } from "lodash";
-import { useSelector } from "react-redux";
-import { RootState } from "../../config/redux/store";
 import styles from "./SettingsView.module.css";
-import { logOutAPI } from "../../helpers/api/users";
 import Profile from "../../components/Profile";
 import placeholder from "../../assets/placeholder.png";
+import globalStyles from "../../App.module.css";
+import ColourBlocks from "./ColourBlocks";
+import { RootState } from "../../config/redux/store";
+import { logOutAPI } from "../../helpers/api/users";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { uploadImage } from "../../config/firebase/config";
-// Semantic UI button
-import globalStyles from "../../App.module.css";
+import { checkAuthAPI } from "../../helpers/api/users";
+import { capitalize, cloneDeep } from "lodash";
 import { updateUserAPI, updatePasswordAPI } from "../../helpers/api/users";
 import { Segment, Icon, Image, Input, Ref, Button } from "semantic-ui-react";
-
-import ColourBlocks from "./ColourBlocks";
-
-import { checkAuthAPI } from "../../helpers/api/users";
 
 const SettingsView = (): JSX.Element => {
     const history = useHistory();
@@ -84,10 +81,16 @@ const UserDetails = (): JSX.Element => {
     const email = user.email;
     const password = "*********";
 
+    const firstNameId = "First name";
+    const lastNameId = "Last name";
+    const emailId = "Email";
+    const passwordId = "Password";
+
     const [editing, setEditing] = useState("");
     const [input, setInput] = useState("");
     const [password1, setPassword1] = useState("");
     const [password2, setPassword2] = useState("");
+
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInput(e.target.value);
     };
@@ -98,11 +101,6 @@ const UserDetails = (): JSX.Element => {
         setPassword2(e.target.value);
     };
 
-    const firstNameId = "First name";
-    const lastNameId = "Last name";
-    const emailId = "Email";
-    const passwordId = "Password";
-
     const inputRef = useRef<HTMLElement | null>(null);
 
     useEffect(() => {
@@ -112,11 +110,18 @@ const UserDetails = (): JSX.Element => {
         });
     });
 
+    // checks which field is being edited and then updates a temp user as
+    // necessary before saving
     const fieldConfirmClick = async () => {
+        if (input == "") {
+            alert("Field cannot be empty");
+            return;
+        }
         const temp = cloneDeep(user);
         switch (editing) {
             case firstNameId:
                 if (input == firstNameId) break;
+
                 temp.firstName = input;
                 updateUserAPI(temp);
                 break;
@@ -157,137 +162,175 @@ const UserDetails = (): JSX.Element => {
         setEditing(name);
     };
 
+    const fieldProps = {
+        editing,
+        input,
+        inputRef,
+        fieldEditClick,
+        fieldConfirmClick,
+        handleInput,
+    };
+
+    const passwordFieldProps = {
+        fieldId: passwordId,
+        value: password,
+        editing,
+        input,
+        inputRef,
+        fieldEditClick,
+        fieldConfirmClick,
+        handlePassword1,
+        handlePassword2,
+    };
+
+    // Each field has a condition to check which field is being edited and render
+    // an input if necessary. Ref is used to handle clicking away from the field
+    // to close
     return (
         <div>
             <div className={styles.formheading}>
                 <div className={styles.formtitle}>
-                    {editing == firstNameId ? (
-                        <Ref innerRef={inputRef}>
-                            <Input
-                                action={{
-                                    icon: "check",
-                                    onClick: fieldConfirmClick,
-                                }}
-                                style={{
-                                    fontSize: "15px",
-                                    marginBottom: "23px",
-                                    marginTop: "-10px",
-                                    width: "92%",
-                                }}
-                                placeholder={input}
-                                onChange={handleInput}
-                            />
-                        </Ref>
-                    ) : (
-                        <div className={styles.field}>
-                            {firstName}
-                            <Icon
-                                size="small"
-                                name="pencil alternate"
-                                color="grey"
-                                onClick={() => fieldEditClick(firstNameId)}
-                            />
-                        </div>
-                    )}
-                    {editing == lastNameId ? (
-                        <Ref innerRef={inputRef}>
-                            <Input
-                                action={{
-                                    icon: "check",
-                                    onClick: fieldConfirmClick,
-                                }}
-                                style={{
-                                    fontSize: "15px",
-                                    marginBottom: "23px",
-                                    marginTop: "-10px",
-                                    width: "92%",
-                                }}
-                                placeholder={input}
-                                onChange={handleInput}
-                            />
-                        </Ref>
-                    ) : (
-                        <div className={styles.field}>
-                            {lastName}
-                            <Icon
-                                size="small"
-                                name="pencil alternate"
-                                color="grey"
-                                onClick={() => fieldEditClick(lastNameId)}
-                            />
-                        </div>
-                    )}
-                    {editing == emailId ? (
-                        <Ref innerRef={inputRef}>
-                            <Input
-                                action={{
-                                    icon: "check",
-                                    onClick: fieldConfirmClick,
-                                }}
-                                style={{
-                                    fontSize: "15px",
-                                    marginBottom: "23px",
-                                    marginTop: "-10px",
-                                    width: "92%",
-                                }}
-                                placeholder={input}
-                                onChange={handleInput}
-                            />
-                        </Ref>
-                    ) : (
-                        <div className={styles.field}>
-                            {email}
-                            <Icon
-                                size="small"
-                                name="pencil alternate"
-                                color="grey"
-                                onClick={() => fieldEditClick(emailId)}
-                            />
-                        </div>
-                    )}
-                    {editing == passwordId ? (
-                        <Ref innerRef={inputRef}>
-                            <span>
-                                <Input
-                                    style={{
-                                        fontSize: "15px",
-                                        marginBottom: "23px",
-                                        marginTop: "-10px",
-                                        width: "37.5%",
-                                    }}
-                                    placeholder={input}
-                                    onChange={handlePassword1}
-                                />
-                                <Input
-                                    action={{
-                                        icon: "check",
-                                        onClick: fieldConfirmClick,
-                                    }}
-                                    style={{
-                                        fontSize: "15px",
-                                        marginBottom: "23px",
-                                        marginLeft: "20px",
-                                        marginTop: "-10px",
-                                        width: "37.5%",
-                                    }}
-                                    placeholder={"Confirm"}
-                                    onChange={handlePassword2}
-                                />
-                            </span>
-                        </Ref>
-                    ) : (
-                        <div className={styles.field}>
-                            {password}
-                            <Icon
-                                size="small"
-                                name="pencil alternate"
-                                color="grey"
-                                onClick={() => fieldEditClick(passwordId)}
-                            />
-                        </div>
-                    )}
+                    <UserField
+                        {...{
+                            fieldId: firstNameId,
+                            value: firstName,
+                            ...fieldProps,
+                        }}
+                    />
+                    <UserField
+                        {...{
+                            fieldId: lastNameId,
+                            value: lastName,
+                            ...fieldProps,
+                        }}
+                    />
+                    <UserField
+                        {...{
+                            fieldId: emailId,
+                            value: email,
+                            ...fieldProps,
+                        }}
+                    />
+                    <PasswordField {...passwordFieldProps} />
                 </div>
             </div>
+        </div>
+    );
+};
+
+type PasswordFieldProps = {
+    editing: string;
+    value: string;
+    input: string;
+    fieldId: string;
+    inputRef: React.MutableRefObject<HTMLElement | null>;
+    fieldEditClick: (name: string) => void;
+    fieldConfirmClick: () => Promise<void>;
+    handlePassword1: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handlePassword2: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+const PasswordField = ({
+    editing,
+    value,
+    input,
+    fieldId,
+    inputRef,
+    fieldEditClick,
+    fieldConfirmClick,
+    handlePassword1,
+    handlePassword2,
+}: PasswordFieldProps): JSX.Element => {
+    if (editing == fieldId)
+        return (
+            <Ref innerRef={inputRef}>
+                <span>
+                    <Input
+                        style={{
+                            fontSize: "15px",
+                            marginBottom: "38px",
+                            marginTop: "-10px",
+                            width: "35.5%",
+                        }}
+                        placeholder={input}
+                        onChange={handlePassword1}
+                    />
+                    <Input
+                        action={{
+                            icon: "check",
+                            onClick: fieldConfirmClick,
+                        }}
+                        style={{
+                            fontSize: "15px",
+                            marginBottom: "38px",
+                            marginTop: "-10px",
+                            marginLeft: "20px",
+                            width: "35.5%",
+                        }}
+                        placeholder={"Confirm"}
+                        onChange={handlePassword2}
+                    />
+                </span>
+            </Ref>
+        );
+    return (
+        <div className={styles.field}>
+            {value}
+            <Button
+                size="small"
+                icon="pencil alternate"
+                onClick={() => fieldEditClick(fieldId)}
+            />
+        </div>
+    );
+};
+
+type UserFieldProps = {
+    editing: string;
+    value: string;
+    input: string;
+    fieldId: string;
+    inputRef: React.MutableRefObject<HTMLElement | null>;
+    fieldEditClick: (name: string) => void;
+    fieldConfirmClick: () => Promise<void>;
+    handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+const UserField = ({
+    editing,
+    value,
+    input,
+    fieldId,
+    inputRef,
+    fieldEditClick,
+    fieldConfirmClick,
+    handleInput,
+}: UserFieldProps): JSX.Element => {
+    if (editing == fieldId)
+        return (
+            <Ref innerRef={inputRef}>
+                <Input
+                    action={{
+                        icon: "check",
+                        onClick: fieldConfirmClick,
+                    }}
+                    style={{
+                        fontSize: "15px",
+                        marginBottom: "38px",
+                        marginTop: "-10px",
+                        width: "89%",
+                    }}
+                    placeholder={input}
+                    onChange={handleInput}
+                />
+            </Ref>
+        );
+    return (
+        <div className={styles.field}>
+            {value}
+            <Button
+                size="small"
+                icon="pencil alternate"
+                onClick={() => fieldEditClick(fieldId)}
+            />
         </div>
     );
 };
@@ -331,7 +374,6 @@ const ProfilePic = (): JSX.Element => {
                 />
                 <input type="file" id="file" hidden onChange={handleFile} />
             </div>
-
             <div className={styles.image}>
                 <Image
                     src={user.profilePic ? user.profilePic : placeholder}
