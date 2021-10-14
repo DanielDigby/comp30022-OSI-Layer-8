0bc272ee0038868912f3e3c86dcf1f1094b1d3c5
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-14 12:40:59 +1100

    feat: add updatePassword route

--- server/src/modules/user/userController.ts
+++ server/src/modules/user/userController.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { hashPassword } from "../../helpers/security";
 import mongoose from "mongoose";
 import { AppError } from "../../helpers/errors";
 import { IRequestWithUser } from "../../interfaces/expressInterfaces";
@@ -29,6 +30,31 @@ const updateUser = async (req: IRequestWithUser, res: express.Response) => {
     }
 };
 
+// controller for updating a user's password
+const updatePassword = async (req: IRequestWithUser, res: express.Response) => {
+    try {
+        const id = req.params.Id;
+        if (id.toString() !== req.user._id.toString())
+            throw new AppError("Unauthorized", 403, "Forbidden", true);
+
+        const { password1, password2 } = req.body;
+        if (password1 !== password2)
+            throw new AppError("Fail", 400, "Non matching passwords", true);
+
+        const user = await User.findById(id);
+        user.password = hashPassword(password1);
+        await user.save();
+        console.log(user);
+        user.password = "redacted";
+
+        return res.status(200).send(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(500);
+    }
+};
+
 module.exports = {
     updateUser,
+    updatePassword,
 };
