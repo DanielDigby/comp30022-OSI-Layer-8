a2a4629bed43e062632cf8234986499c2e8c34e3
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-15 20:40:17 +1100

    feat: add editing to newNote

--- client/src/views/NotesView/NewNote/index.tsx
+++ client/src/views/NotesView/NewNote/index.tsx
@@ -1,12 +1,41 @@
-import React from "react";
-import "semantic-ui-css/semantic.min.css";
+import React, { useState } from "react";
 import { Segment, Icon } from "semantic-ui-react";
+import { INote, NoteModes } from "../../../interfaces/note";
+import Note from "../../../components/Note";
 import styles from "./NewNote.module.css";
 
 const NewNote = (): JSX.Element => {
+    const [isEditing, toggleEditing] = useState(false);
+    const emptyNote: INote = {
+        title: null,
+        user: null,
+        _id: null,
+        _clientId: "",
+        text: null,
+        image: null,
+        reminderTime: null,
+        eventTime: null,
+        pinned: false,
+        tags: [],
+        relatedNotes: [],
+    };
+
+    const toggleOn = () => {
+        toggleEditing(true);
+    };
+    const toggleOff = () => {
+        toggleEditing(false);
+    };
+
     // api call
+    if (isEditing)
+        return (
+            <div onClick={toggleOff}>
+                <Note note={emptyNote} mode={NoteModes.EDIT} />
+            </div>
+        );
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onClick={toggleOn}>
             <Segment.Group raised>
                 <Segment>
                     <div className={styles.body}>
