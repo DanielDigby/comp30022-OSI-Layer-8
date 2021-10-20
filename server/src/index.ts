236e2f409589748b939575d5982ea729fff1a0b2
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-20 13:06:11 +1100

    feat: set up backend sentry error logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import cors from "cors";
 import path from "path";
 import helmet from "helmet";
@@ -10,6 +12,25 @@ require("./config/passport");
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+Sentry.init({
+    dsn: "https://f076062d3e124a698c7171548e7a246e@o1044993.ingest.sentry.io/6020267",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    tracesSampleRate: 1.0,
+});
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // middleware
 app.use(
@@ -58,6 +79,9 @@ app.use("/api/users/", userRouter);
 const authRouter = require("./modules/auth/authRouter");
 app.use("/api/auth/", authRouter);
 
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+// local error handler
 app.use(errorHandler);
 
 // Frontend connection
