c1a06e881e70c46e85e2ca146f3b9271a436d385
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-10-21 09:35:45 +1100

    feat: add upload image helper function

--- client/src/helpers/api/notes.ts
+++ client/src/helpers/api/notes.ts
@@ -1,5 +1,7 @@
-import { INote, INoteWithoutIds } from "../../interfaces/note";
 import { store } from "../../config/redux/store";
+import { storage } from "../../config/firebase/config";
+import { INote, INoteWithoutIds } from "../../interfaces/note";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import {
     createNote,
     updateNote,
@@ -16,4 +18,27 @@ export const updateNoteAPI = (note: INote): void => {
 
 export const deleteNoteAPI = (note: INote): void => {
     store.dispatch(deleteNote(note));
+};
+
+export const uploadNoteImageAPI = async (
+    note: INote,
+    fileList: FileList | null
+): Promise<void> => {
+    if (!fileList) throw new Error("No file selected");
+
+    if (fileList[0]) {
+        const file = fileList[0];
+        const fileType = file["type"];
+        const validImageTypes = ["image/jpeg", "image/png"];
+        if (validImageTypes.includes(fileType)) {
+            const fileRef = ref(storage, `images/${file.name}`);
+            await uploadBytes(fileRef, file);
+            const url = await getDownloadURL(fileRef);
+
+            if (!url) throw new Error("Failed upload");
+
+            note.image = url;
+            store.dispatch(updateNote(note));
+        }
+    }
 };
