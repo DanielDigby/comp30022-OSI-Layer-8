039fa1f670be806202927a120699bdc1b0f31ffd
XRAYRAY22 <88013109+XRAYRAY22@users.noreply.github.com>
2021-10-21 20:52:20 +1100

    fix: Log in tests now checks for dashboard

--- cypress/integration/acc-10.js
+++ cypress/integration/acc-10.js
@@ -1,14 +1,15 @@
-describe('10. User can add a filter', () => {
-  
-    it('Can add a filter to the list of filters on the right side', () => {
-      // can use this register hook to auth a user before doing the rest of the testing
-      cy.register()
-      cy.visit('/notes')
+describe("10. User can add a filter", () => {
+  it("Can add a filter to the list of filters on the right side", () => {
+    // can use this register hook to auth a user before doing the rest of the testing
+    cy.register();
+    cy.visit("/notes");
 
-      // add a filter
-      cy.get('*[class^="plus large icon"]').click()
-      cy.get('#input-tag').type("new tag")
-      cy.get('*[class^="plus icon"]').click()
-      cy.get('*[class^="ui fluid secondary vertical menu"]').children().contains('New Tag')
-    })
-  })
+    // add a filter
+    cy.get('*[class^="plus large icon"]').click();
+    cy.get("#input-tag").type("new tag");
+    cy.get('*[class^="plus icon"]').click();
+    cy.get('*[class^="ui fluid secondary vertical menu"]')
+      .children()
+      .contains("New Tag");
+  });
+});
--- cypress/integration/acc-2.js
+++ cypress/integration/acc-2.js
@@ -4,13 +4,11 @@ describe("1. A user can log in", () => {
     "Navigates to the login page and fills in necessary details." +
       "Succedes when navigated from login url to dashboard",
     () => {
-      // Goes to the dashboard page
       cy.register();
-      cy.visit("/dashboard");
+      cy.visit("/");
 
-      cy.get("EventNote_Segment__oFjQB");
-
-      cy.get("DashboardView_pinnedNotesContainer__2SmGG");
+      // Check that we are on the dashboard page
+      cy.url().should("be.equal", "http://localhost:3000/");
     }
   );
 });
